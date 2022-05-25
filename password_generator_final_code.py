77850cfcbe3823daa7e967d3e338cf9fec82ef29
Ddembe Galiwango Humphrey <galiwangohumphrey55@gmail.com>
2022-05-25 21:03:55 +0300

    using python to generated a password randomly.
    This program generates a password of any length depending on the user's choice. the password contains symbols, digits and characters.

---
+++ password_generator_final_code.py
@@ -0,0 +1,95 @@
+"""
+    Password Generator Project
+    input:  password length
+            Number of Digits
+            Number of Symbols
+    
+    output: password (as string)
+"""
+####################################################################################################
+######################## DO NOT CHANGE THIS SECTION OF THE CODE ####################################
+# importing the relevant modules
+from ntpath import join
+import string
+import random
+
+# List of all lowercase and uppercase characters in Python
+all_alphabets = list(string.ascii_letters)
+# print(f'these are the alphebets of the english lang. {all_alphabets}')
+
+all_digits = list(string.digits)
+# print(f'These are the numbers in english lang: {all_digits}')
+
+all_symbols = list(string.punctuation)
+# print(f'These are all the symbols: in the english lang: {all_symbols}')
+ 
+#########################################################################################################
+
+
+
+####################################### TYPE YOU CODE IN HERE! ##########################################
+# Ask user for how many letters they would like in their password and cast to integer and store as no_of_letters
+no_of_letters = int(input("How many letters would you like in your password? "))
+ 
+# Ask user for how many symbols they would like in their password and cast to integer and store as no_of_symbols
+no_of_symbols = int(input("How many symbols would you like in your password? "))
+
+# Ask user for how many digits they would like in their password and cast to integer and store as no_of_digits
+no_of_digits = int(input("How many digits would you like in your password? "))
+
+
+# Set Accumulator for Password Characters List
+password_character_list = [] 
+
+# Get the random letter for the password
+# Randomly Select the characters
+for number in range(no_of_letters):
+    #   select a random characer from list of alphabets and append to the password characters list 
+    password_character_list.append(random.choice(all_alphabets))
+
+
+# Get the Random Symbols for the password symbols
+
+# Set Accumulator for the Number of Symbols List
+password_symbol_list = []
+
+# Randomly Select the characters
+for number in range(no_of_symbols):
+    #   select a random symbol from list of symbols and append to the password symbols list 
+    password_symbol_list.append(random.choice(all_symbols))
+
+
+# Get the Random Digits for the password
+
+# Set Accumulator for the Number of Digits List
+password_digit_list = []
+# Randomly Select the characters
+for number in range(no_of_digits):
+    #   select a random digit from list of digits and append to the password digits list 
+    password_digit_list.append(random.choice(all_digits))
+
+
+# Add the lists to get the final_password_list and shuffle the final_password_list
+
+final_password_list = password_character_list + password_symbol_list + password_digit_list
+
+
+# To shuffle, items in a list https://www.w3schools.com/python/ref_random_shuffle.asp 
+random.shuffle(final_password_list)
+
+# Now that we have our password in a list lets change that to a string then print it
+
+final_password_list = ''.join(final_password_list)
+
+
+
+# accumulator for string_password
+string_password = ''
+
+for character in final_password_list:
+    #append the character to the string_password
+    string_password += character
+    
+
+# Print the string password
+print(f'Here is your password: {string_password}')
